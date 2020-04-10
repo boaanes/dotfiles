8527504de88680879577ed66d3ebe12adfc59ea4
Bo Aanes <boaanes@gmail.com>
2020-04-10 18:04:28 +0200

    modified temp script with dynamic icon

--- .config/polybar/scripts/temp.sh
+++ .config/polybar/scripts/temp.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 TEMP=$(sensors | grep "Package id 0:" | cut -d ' ' -f 5 | cut -d '+' -f 2- | cut -d '.' -f 1)
+ICON=""
 
-printf " $TEMP°C"
+if [[ $TEMP -gt 80 ]]; then
+    ICON=""
+elif [[ $TEMP -gt 60 ]]; then
+    ICON=""
+elif [[ $TEMP -gt 45 ]]; then
+    ICON=""
+elif [[ $TEMP -gt 30 ]]; then
+    ICON=""
+else
+    ICON=""
+fi
+
+printf "$ICON $TEMP°C"
