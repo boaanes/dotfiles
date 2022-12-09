d1011c8593e883be69b3841dddc155b6b6e9cf9e
Bo Aanes <boaanes@gmail.com>
2022-12-09 14:34:19 +0100

    remove simple tab completion, fix wrong attach name

--- .config/nvim/lua/lsp_config_wrapper.lua
+++ .config/nvim/lua/lsp_config_wrapper.lua
@@ -81,33 +81,6 @@ cmp.setup {
             vim.fn["vsnip#anonymous"](args.body)
         end,
     },
-    mapping = cmp.mapping.preset.insert({
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-                elseif vim.fn["vsnip#available"](1) == 1 then
-                    feedkey("<Plug>(vsnip-expand-or-jump)", "")
-                elseif has_words_before() then
-                    cmp.complete()
-                else
-                    fallback()
-            end
-        end, { "i", "s" }),
-
-        ["<S-Tab>"] = cmp.mapping(function()
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-                feedkey("<Plug>(vsnip-jump-prev)", "")
-            end
-        end, { "i", "s" }),
-
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    }),
     sources = {
         { name = 'nvim_lsp' }
     }
@@ -124,8 +97,8 @@ require('mason-lspconfig').setup_handlers({
     end,
     ["eslint"] = function()
         require('lspconfig')[eslint].setup {
-            on_attach = on_attach,
-            capabilities = capabilities,
+            on_attach = custom_lsp_attach,
+            capabilities = cmp_capabilities,
             cmd = { "eslint_d", "--stdin" }
         }
     end,
@@ -147,12 +120,12 @@ require('mason-lspconfig').setup_handlers({
 })
 
 require('lspconfig').hls.setup {
-    on_attach = on_attach,
+    on_attach = custom_lsp_attach,
     settings = {
         haskell = {
             formattingProvider = "stylish-haskell"
         }
     },
-    capabilities = capabilities
+    capabilities = cmp_capabilities
 }
 
