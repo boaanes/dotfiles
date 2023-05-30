270fa78509317fba75848feb30074e91b5c54fe1
Bo Aanes <boaanes@gmail.com>
2023-05-30 15:32:35 +0200

    Update various plugin commits and add barbecue.nvim with dependencies

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -135,4 +135,16 @@ return {
   {
     "neovimhaskell/haskell-vim",
   },
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {
+      -- configurations go here
+    },
+  },
 }
