ce058a935a36b73e9367cea1476f4a805d514149
Bo Aanes <boaanes@gmail.com>
2023-06-19 19:56:14 +0200

    Remove TokyoNight color scheme from nvim config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,26 +3,6 @@ return {
     "sainnhe/sonokai",
   },
   {
-    "folke/tokyonight.nvim",
-    -- lazy = false,
-    -- priority = 1000,
-    -- opts = function()
-    --   return {
-    --     style = "storm",
-    --     lualine_bold = true,
-    --     styles = {
-    --       comments = { italic = true },
-    --       keywords = { italic = true },
-    --     },
-    --   }
-    -- end,
-    -- config = function(_, opts)
-    --   local tokyo = require("tokyonight")
-    --   tokyo.setup(opts)
-    --   tokyo.load()
-    -- end,
-  },
-  {
     "navarasu/onedark.nvim",
     config = function()
       require("onedark").setup({
